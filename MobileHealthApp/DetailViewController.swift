b5cf4a88bf00668d594d7a1e67854dc7e500a1d1
Ben Ashkenazi <ben.ashkenazi@gmail.com>
2023-07-19 16:58:21 -0700

    First Commit

--- MobileHealthApp/DetailViewController.swift
+++ MobileHealthApp/DetailViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class DetailViewController: UIViewController {
     var unit: HealthUnit?
+
     
     override func viewDidLoad(){
         super.viewDidLoad()
+         
+
+        /*if let dayOfWeek = calculateDayOfWeek(year: year, month: month, day: day) {
+            print("The day of the week is:", dayOfWeek)
+        } else {
+            print("Invalid date.")
+        }*/
+        
         
         let screenSize: CGRect = UIScreen.main.bounds
         let screenWidth = screenSize.width
@@ -21,15 +30,20 @@ class DetailViewController: UIViewController {
         self.view.backgroundColor = UIColor(red:127.0/255.0, green:86.0/255.0, blue:108.0/255.0, alpha: 1.0)
 
 
-        let nameLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
-        nameLabel.font = UIFont.preferredFont(forTextStyle: .footnote)
+        let nameLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 400, height: 40))
+        nameLabel.font = UIFont.boldSystemFont(ofSize: screenHeight/25)
         nameLabel.numberOfLines = 0
         nameLabel .textColor = .black
         nameLabel .center = CGPoint(x: screenWidth/2.0, y: screenHeight/6)
         nameLabel .textAlignment = .center
         nameLabel .text = unit?.name
-        
-        
+        let attributedString = NSAttributedString(string: nameLabel.text ?? "",
+                                                         attributes: [
+                                                           NSAttributedString.Key.strokeColor: UIColor.white,
+                                                           NSAttributedString.Key.strokeWidth: -0.25
+                                                         ])
+       nameLabel.attributedText = attributedString
+
         /*let isoDateString = (unit?.open)!
         let dateFormatter = ISO8601DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
@@ -48,39 +62,57 @@ class DetailViewController: UIViewController {
        
         
         let hoursLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 400, height: 21))
-        hoursLabel.font = UIFont.preferredFont(forTextStyle: .footnote)
+        hoursLabel.font =  UIFont.boldSystemFont(ofSize: screenHeight/40)
         hoursLabel.numberOfLines = 0
         hoursLabel .textColor = .black
         hoursLabel .center = CGPoint(x: screenWidth/2.0, y: screenHeight/5)
         hoursLabel .textAlignment = .center
-        
-        hoursLabel .text = "\(String(describing: (unit?.rawopen)!)) - \(String(describing: (unit?.rawclose)!))"
+        hoursLabel .text = "Hours: \(String(describing: (unit?.open)!)) - \(String(describing: (unit?.close)!))"
         //hoursLabel .text = "\(Date.dateFromISOString(string: (unit?.open)!))"
         
+        
         //Days Open Label
-        let daysLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 400, height: 21))
-        daysLabel.font = UIFont.preferredFont(forTextStyle: .footnote)
+        let avaLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 400, height: 50))
+        avaLabel.font = UIFont.boldSystemFont(ofSize: screenHeight/50)
+        avaLabel.numberOfLines = 0
+        avaLabel .textColor = .black
+        avaLabel .center = CGPoint(x: screenWidth/2.0, y: screenHeight/3.5)
+        avaLabel .textAlignment = .center
+        avaLabel .text = "Days Available This Month:"
+        
+        //Days Open Label
+        let daysLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 400, height: 50))
+        daysLabel.font = UIFont.boldSystemFont(ofSize: screenHeight/60)
         daysLabel.numberOfLines = 0
         daysLabel .textColor = .black
-        daysLabel .center = CGPoint(x: screenWidth/2.0, y: screenHeight/4.5)
+        daysLabel .center = CGPoint(x: screenWidth/2.0, y: screenHeight/3)
         daysLabel .textAlignment = .center
-        daysLabel .text = "\((unit?.rawdays)!)"
+        var daysTxt = ""
+        for day in (unit?.days)!{
+            daysTxt = daysTxt+"\((unit?.MonthYear)!.suffix(2))/\(day!) "
+        }
+        daysLabel .text = daysTxt
         //Days open july?
         
         //Address
-        let addrLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 400, height: 21))
-        addrLabel.font = UIFont.preferredFont(forTextStyle: .footnote)
+        let addrLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 100))
+        addrLabel.font = UIFont.boldSystemFont(ofSize: 18.0)
         addrLabel.numberOfLines = 0
         addrLabel .textColor = .black
-        addrLabel .center = CGPoint(x: screenWidth/2.0, y: screenHeight/4)
-        addrLabel .textAlignment = .center
-        addrLabel .text = unit?.address
+        addrLabel .center = CGPoint(x: screenWidth/3, y: screenHeight/1.45)
+        addrLabel .textAlignment = .left
+        addrLabel.isUserInteractionEnabled = true
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(labelTapped(_:)))
+        addrLabel.addGestureRecognizer(tapGesture)
+        addrLabel .text = (unit?.address)!.replacingOccurrences(of: ",", with: ",\n")
 
         //apple maps button
-        let openMapsButton = UIButton(type: .system)
+        let openMapsButton = UIButton(type: .custom)
+        openMapsButton.setImage(UIImage(named: "appmaps"), for: .normal)
+        
         openMapsButton.setTitle("Open in Maps", for: .normal)
         openMapsButton.addTarget(self, action: #selector(openMaps), for: .touchUpInside)
-        openMapsButton.frame = CGRect(x: 100, y: screenHeight/3.25, width: 200, height: 50)
+        openMapsButton.frame = CGRect(x: screenWidth/1.5, y: screenHeight/1.55, width: screenHeight/10, height: screenHeight/10)
         view.addSubview(openMapsButton)
         
         
@@ -88,26 +120,41 @@ class DetailViewController: UIViewController {
         
         
         //phone call button
-        let callButton = UIButton(type: .system)
-       callButton.setTitle("Call", for: .normal)
-       callButton.addTarget(self, action: #selector(makeCall), for: .touchUpInside)
-        callButton.frame = CGRect(x: 100, y: screenHeight/3, width: 200, height: 50)
+        let callButton = UIButton(type: .custom)
+        callButton.setImage(UIImage(named: "call"), for: .normal)
+        callButton.setTitle("Call", for: .normal)
+        callButton.addTarget(self, action: #selector(makeCall), for: .touchUpInside)
+        callButton.frame = CGRect(x: screenWidth/5.75, y: screenHeight/1.15, width: screenHeight/14, height: screenHeight/14)
        
-        let sButton = UIButton(type: .system)
+        //survey button
+        let sButton = UIButton(type: .custom)
+        sButton.setImage(UIImage(named: "survey"), for: .normal)
        sButton.setTitle("Survey Form", for: .normal)
        sButton.addTarget(self, action: #selector(openSurveyForm), for: .touchUpInside)
-        sButton.frame = CGRect(x: 100, y: screenHeight/2.75, width: 200, height: 50)
-        //email button
+        sButton.frame = CGRect(x: screenWidth/1.45, y: screenHeight/1.15, width: screenHeight/17, height: screenHeight/14)
+
+        //faq button
+        let faqButton = UIButton(type: .custom)
+        faqButton.setTitle("FAQ", for: .normal)
+        faqButton.setTitleColor(.white, for: .normal)
+        let faqColor = UIColor(red: 44.0/255.0, green: 101.0/255.0, blue: 78.0/255.0, alpha: 1.0)
+        faqButton.backgroundColor = faqColor
+        faqButton.layer.borderWidth = 2.0
+        faqButton.layer.borderColor = UIColor.white.cgColor
+        faqButton.layer.cornerRadius = 10.0
+        faqButton.frame = CGRect(x: screenWidth/2.5, y: screenHeight/2, width: screenHeight/10, height: screenHeight/14)
+
         
-        //survey form button
         
 
         self.view.addSubview(nameLabel)
         self.view.addSubview(hoursLabel)
+        self.view.addSubview(avaLabel)
         self.view.addSubview(daysLabel)
         self.view.addSubview(addrLabel)
         self.view.addSubview(callButton)
         self.view.addSubview(sButton)
+        self.view.addSubview(faqButton)
     }
     
     @objc func openMaps() {
@@ -142,25 +189,30 @@ class DetailViewController: UIViewController {
         UIApplication.shared.open(surveyURL)
     }
     
-}
+    @objc func labelTapped(_ gesture: UITapGestureRecognizer) {
+           guard let label = gesture.view as? UILabel else {
+               return
+           }
 
-extension Date {
-    static func ISOStringFromDate(date: Date) -> String {
-        let dateFormatter = DateFormatter()
-        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
-        dateFormatter.timeZone = TimeZone(abbreviation: "GMT")
-        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS"
-        
-        return dateFormatter.string(from: date).appending("Z")
+           // Copy the label's text to the clipboard
+           UIPasteboard.general.string = label.text
+       }
+   }
+
+func calculateDayOfWeek(year: Int, month: Int, day: Int) -> String? {
+    let calendar = Calendar.current
+    let components = DateComponents(year: year, month: month, day: day)
+    
+    guard let date = calendar.date(from: components) else {
+        return nil
     }
     
-    static func dateFromISOString(string: String) -> Date? {
-        let dateFormatter = DateFormatter()
-        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
-        dateFormatter.timeZone = TimeZone(abbreviation: "GMT")
-        //dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
-        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
-        
-        return dateFormatter.date(from: string)
-    }
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "EEEE"
+    
+    let dayOfWeek = dateFormatter.string(from: date)
+    
+    return dayOfWeek
 }
+
+
--- MobileHealthApp/HealthUnit.swift
+++ MobileHealthApp/HealthUnit.swift
@@ -6,11 +6,15 @@
 //
 
 import Foundation
+import MapKit
+import CoreLocation
+
 class HealthUnit {
     var id: Int?
     //iso 8601
     var MonthYear: String?
     var name: String?
+    var partner: String?
     var number: URL?
     
     //iso 8601
@@ -18,9 +22,9 @@ class HealthUnit {
     //iso 8601
     var close: String?
     
+    var rawMY: String?
     var rawopen: String?
     var rawclose: String?
-    
     var rawdays: String?
     
     var days: [Int?]
@@ -29,44 +33,49 @@ class HealthUnit {
     
     var comments: String?
     
-    init(rawId: String, rawMY: String, name: String, rawnumber: String, rawopen: String, rawclose: String, rawdays: String, rawaddr: String, comments: String?) {
-       
+    init(rawId: String, rawMY: String, name: String, rawpartner: String,rawnumber: String, rawopen: String, rawclose: String, rawdays: String, rawaddr: String, comments: String?) {
+        
         let formatter = ISO8601DateFormatter()
         formatter.formatOptions = [.withInternetDateTime]
-
+        
         //sets id, 0 if there was an error
         self.id = Int(rawId) ?? 0
         
         //sets month/year
+        self.rawMY = rawMY
         self.MonthYear = String(rawMY.prefix(7))
-
+        
         //sets name
         self.name = name
         
+        self.partner = rawpartner
         //sets phone number
         //var num = rawnumber.replacingOccurrences(of: ")(- ", with: "", options: NSString.CompareOptions.literal, range: nil)
         self.number = URL(string: "tel://" + String(rawnumber)) ?? URL(string: "tel://" + "0000000000")
-        print(rawnumber)
-        
+                    
         //gets opening hours
-        self.open = rawopen
-        print("Raw Opening Time: \(rawopen)")
-        //gets closing hours
-        self.close = rawclose
+        self.rawopen = convertToArizonaTime(from: rawopen)
+        self.rawclose = convertToArizonaTime(from: rawclose)
         
-        self.rawopen = String(rawopen.prefix(16).suffix(5))
-        self.rawclose = String(rawclose.prefix(16).suffix(5))
-        
-        //self.rawopen = rawopen
-        //self.rawclose = rawclose
+  
+        self.open = String(String(String(self.rawopen!).prefix(16)).suffix(5))
+        self.close = String(String(String(self.rawclose!).prefix(16)).suffix(5))
         
         
-       
         //gets days
         var nospace = rawdays.replacingOccurrences(of: " ", with: "", options: NSString.CompareOptions.literal, range: nil)
-        var dayArray = nospace.split(separator: " ")
-        self.rawdays = rawdays
-        self.days = convertStringsToInts(array: dayArray)
+
+        if(nospace.count<3){
+            var dayArray = [nospace.prefix(2)]
+            self.rawdays = rawdays
+            self.days = convertStringsToInts(array: dayArray)
+        }else{
+            var dayArray = nospace.split(separator: ",")
+            self.rawdays = rawdays
+            self.days = convertStringsToInts(array: dayArray)
+        }
+        
+        print(self.days)
         
         //gets address
         self.address = rawaddr
@@ -76,9 +85,67 @@ class HealthUnit {
     
     func toString()->String{
         let dateFormatter = DateFormatter()
-        
-        
         return "\(name ?? "lol"): \(MonthYear ?? "4/23")\n Days Open: \(rawdays!)\n\(address!)\n\(number!)"
+    }
+    
+    func isComplete()->Bool{
+        if(id==0){
+            return false
+        }
+        if(name == ""){
+            return false
+        }
+        if(rawdays == ""){
+            return false
+        }
+        if (rawopen == ""){
+            return false
+        }
+        if(number == nil){
+            return false
+        }
+        if(address == nil){
+            return false
+        }
+        return true
+    }
+    
+    func location(completion: @escaping (CLLocationCoordinate2D?) -> Void) {
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(address!) { (placemarks, error) in
+            guard let coordinate = placemarks?.first?.location?.coordinate else {
+                print("Failed to get location for address: \(self.address)")
+                completion(nil)
+                return
+            }
+            print("Got location for address: \(self.address)")
+            completion(coordinate)
+        }
+    }
+    
+    func isOpen(on date: Date) -> Bool {
+        let calendar = Calendar.current
+        let day = calendar.component(.weekday, from: date)
+        let hour = calendar.component(.hour, from: date)
+        
+        print("Checking if \(name) is open on day \(day) at hour \(hour)")
+                
+        if !days.contains(day) || hour < timeToInt(timeString: open!) || hour > timeToInt(timeString: close!) {
+            return false
+        }
+                
+        return true
+    }
+    
+    func timeToInt(timeString: String)-> Int{
+        let components = timeString.split(separator: ":")
+        if components.count == 2, let hour = Int(components[0]) {
+            print("The number is: \(hour)")
+            return hour
+        } else {
+            print("Invalid time format")
+            return 0
+        }
     }
 }
 
@@ -87,3 +154,18 @@ func convertStringsToInts(array: [String.SubSequence]) -> [Int?] {
     return convertedArray
 }
 
+func convertToArizonaTime(from iso8601String: String) -> String? {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+    
+    guard let date = dateFormatter.date(from: iso8601String) else {
+        return nil
+    }
+    
+    let arizonaTimeZone = TimeZone(identifier: "America/Phoenix")
+    dateFormatter.timeZone = arizonaTimeZone
+    
+    let arizonaDate = dateFormatter.string(from: date)
+    
+    return arizonaDate
+}
